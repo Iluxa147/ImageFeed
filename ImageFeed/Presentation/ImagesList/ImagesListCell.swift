ebefbc7c82aeda17d9efaff05e3dd6867731742f
Iluxa147 <pokolewIlja@gmail.com>
2026-03-30 14:29:17 +0300

    feat: add table cell storyboard and art for it

---
+++ ImageFeed/Presentation/ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Ilya Pokolev on 30.03.2026.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
--- ImageFeed/Presentation/ImagesList/ImagesListViewController.swift
+++ ImageFeed/Presentation/ImagesList/ImagesListViewController.swift
@@ -11,9 +11,18 @@ final class ImagesListViewController: UIViewController {
     // MARK: - UI
     @IBOutlet private var tableView: UITableView!
     
+    // MARK: - State
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        
     }
 }
 
