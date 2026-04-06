45ca8a8577e0eed3594d4567b2fc2d3cb056b7c5
Iluxa147 <pokolewIlja@gmail.com>
2026-04-06 03:10:19 +0300

    feat: single image view wip

---
+++ ImageFeed/Presentation/SingleImageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Ilya Pokolev on 06.04.2026.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    // MARK: - UI
+    
+    @IBOutlet private weak var imageViewSplash: UIImageView!
+    
+    
+}
